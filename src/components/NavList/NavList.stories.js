59bf0c72b808fb3364e341fc3158e7daccc05c8c
Matt Kane <ascorbic@gmail.com>
2018-06-14 12:09:53 +0100

    Fix error in storybook

--- src/components/NavList/NavList.stories.js
+++ src/components/NavList/NavList.stories.js
@@ -1,27 +1,27 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import { AnimalList } from "./AnimalList";
+import { NavList } from "./NavList";
 import animals from "../../fixtures/stripes.json";
 import "../../theme/theme.css";
 
 storiesOf("AnimalList", module)
     .add("First item selected", () => (
-        <AnimalList
+        <NavList
             animals={animals}
             onClick={action("Clicked item")}
             selectedAnimal={animals[0]}
         />
     ))
     .add("Second item selected", () => (
-        <AnimalList
+        <NavList
             animals={animals}
             onClick={action("Clicked item")}
             selectedAnimal={animals[1]}
         />
     ))
     .add("Last item selected", () => (
-        <AnimalList
+        <NavList
             animals={animals}
             onClick={action("Clicked item")}
             selectedAnimal={animals[animals.length - 1]}
