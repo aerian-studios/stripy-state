0e6a9f2117dcc9c65975cb592b205ddb44c3e0dc
Matt Kane <ascorbic@gmail.com>
2018-06-14 11:57:43 +0100

    Add types

---
+++ types.d.ts
@@ -0,0 +1,12 @@
+export interface Animal {
+    uuid: string;
+    type: string;
+    isPage: boolean;
+    common_name: string;
+    description: string;
+    image_full: string;
+    latin_name: string;
+    image_thumb: string;
+
+    conservation_status: string;
+}
